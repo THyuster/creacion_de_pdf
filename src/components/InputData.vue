8f24f8db71b8e7b20d8cd0c655f2d3e5374de62c
Jhon Rodríguez <157409281+Jhon-Rodriguez20@users.noreply.github.com>
2024-03-01 10:03:46 -0500

    Correccion en los estilos de las páginas que se generan

--- src/components/InputData.vue
+++ src/components/InputData.vue
@@ -122,83 +122,94 @@ export default {
       this.disposicionFinal = "f";
     },
     async generarPDF() {
-      const jsPDF = await import('jspdf');
-      const doc = new jsPDF.default();
+  const jsPDF = await import('jspdf');
+  const doc = new jsPDF.default();
 
-      doc.setDrawColor(0);
-      doc.setLineWidth(0.2);
-      doc.setFontSize(8);
+  doc.setDrawColor(0);
+  doc.setLineWidth(0.2);
+  doc.setFontSize(8);
 
-      // Función para dibujar los márgenes de la página
-      function drawBorders(doc) {
-        doc.line(5, 5, doc.internal.pageSize.width - 5, 5);
-        doc.line(5, 5, 5, doc.internal.pageSize.height - 5);
-        doc.line(5, 292, doc.internal.pageSize.width - 5, 292);
-        doc.line(205, 5, 205, doc.internal.pageSize.height - 5);
-      }
+  // Función para dibujar los márgenes de la página
+  function drawBorders(doc) {
+    doc.line(5, 5, doc.internal.pageSize.width - 5, 5);
+    doc.line(5, 5, 5, doc.internal.pageSize.height - 5);
+    doc.line(5, 292, doc.internal.pageSize.width - 5, 292);
+    doc.line(205, 5, 205, doc.internal.pageSize.height - 5);
+  }
 
-      // Dibujar el encabezado
-      function drawHeader(doc) {
-        doc.addImage(imagenData, 'JPEG', 6, 6, 38, 20);
-        doc.text('Direccion De Mantenimiento', 65, 13)
-        doc.text('Actas De Mantenimiento Electrico', 61, 23)
-        doc.text('NIT', 138, 10);
-        doc.text('123456789-0', 132, 15);
-        doc.text('Código de Factura', 128, 20);
-        doc.text('ABC123', 135, 27)
-        const fechaActual = new Date().toLocaleDateString();
-        doc.text('Fecha', 177, 21)
-        doc.text(`${fechaActual}`, 175, 27);
-        const totalPages = doc.internal.getNumberOfPages();
-        doc.text('Paginas', 176, 9)
-        for (let i = 1; i <= totalPages; i++) {
-          doc.setPage(i);
-          doc.text(`${i} de ${totalPages}`, doc.internal.pageSize.width - 33, 15);
-        }
-        doc.line(5, 28, doc.internal.pageSize.width - 5, 28);
-        doc.line(46, 17, doc.internal.pageSize.width - 5, 17);
-        doc.line(46, 5, 46, doc.internal.pageSize.height - 269);
-        doc.line(120, 5, 120, doc.internal.pageSize.height - 269);
-        doc.line(120, 11, doc.internal.pageSize.width - 5, 11);
-        doc.line(120, 23, doc.internal.pageSize.width - 5, 23);
-        doc.line(160, 5, 160, doc.internal.pageSize.height - 269);
-      }
-
-      // Dibujar el encabezado y los márgenes en la primera página
-      drawBorders(doc);
-      drawHeader(doc);
-
-      doc.autoTable({
-        startY: 28.5,
-        head: [['Nombre del EPP', 'Parte del Cuerpo a Proteger', 'Riesgo Controlado', 'Cargo Asociado', 'Especificación Técnica', 'Uso', 'Mantenimiento', 'Vida Útil', 'Reposición', 'Disposición Final']], // Encabezado de la tabla
-        body: this.items.map(item => [
-          item.nombreEpp,
-          item.parteCuerpoProteger,
-          item.riesgoControlado,
-          item.cargoAsociado,
-          item.especificacionTecnica,
-          item.uso,
-          item.mantenimiento,
-          item.vidaUtil,
-          item.reposicion,
-          item.disposicionFinal
-        ]), // Datos de la tabla
-        theme: 'grid', // Estilo de la tabla
-        margin: { top: 28.5, left: 5.5, right: 5.5}, // Margen superior para evitar que se solape con el encabezado
-        styles: { fontSize: 8 }, // Estilo de fuente para la tabla
-        headerStyles: {
-          textColor: '#000',
-          fillColor: [200, 200, 200], // Color gris (F4F4F4)
-        },
-        columnWidth: 'auto' // Anchura automática para la primera columna
-      });
-
-      // Dibujar el encabezado y los márgenes en la segunda página
-      drawHeader(doc);
-      drawBorders(doc);
-
-      doc.save("informacion.pdf");
+  // Dibujar el encabezado
+  function drawHeader(doc) {
+    doc.addImage(imagenData, 'JPEG', 6, 6, 38, 20);
+    doc.text('Direccion De Mantenimiento', 65, 13)
+    doc.text('Actas De Mantenimiento Electrico', 61, 23)
+    doc.text('NIT', 138, 10);
+    doc.text('123456789-0', 132, 15);
+    doc.text('Código de Factura', 128, 20);
+    doc.text('ABC123', 135, 27)
+    const fechaActual = new Date().toLocaleDateString();
+    doc.text('Fecha', 177, 21)
+    doc.text(`${fechaActual}`, 175, 27);
+    const totalPages = doc.internal.getNumberOfPages();
+    doc.text('Paginas', 176, 9)
+    for (let i = 1; i <= totalPages; i++) {
+      doc.setPage(i);
+      doc.text(`${i} de ${totalPages}`, doc.internal.pageSize.width - 33, 15);
     }
+    doc.line(5, 28, doc.internal.pageSize.width - 5, 28);
+    doc.line(46, 17, doc.internal.pageSize.width - 5, 17);
+    doc.line(46, 5, 46, doc.internal.pageSize.height - 269);
+    doc.line(120, 5, 120, doc.internal.pageSize.height - 269);
+    doc.line(120, 11, doc.internal.pageSize.width - 5, 11);
+    doc.line(120, 23, doc.internal.pageSize.width - 5, 23);
+    doc.line(160, 5, 160, doc.internal.pageSize.height - 269);
+  }
+
+  // Dibujar el encabezado y los márgenes en la primera página
+  drawBorders(doc);
+  drawHeader(doc);
+
+  // Guardar la posición y el número de página actual
+  const firstPagePosition = doc.internal.getCurrentPageInfo().pageNumber;
+
+  doc.autoTable({
+    startY: 28.5,
+    head: [['Nombre del EPP', 'Parte del Cuerpo a Proteger', 'Riesgo Controlado', 'Cargo Asociado', 'Especificación Técnica', 'Uso', 'Mantenimiento', 'Vida Útil', 'Reposición', 'Disposición Final']], // Encabezado de la tabla
+    body: this.items.map(item => [
+      item.nombreEpp,
+      item.parteCuerpoProteger,
+      item.riesgoControlado,
+      item.cargoAsociado,
+      item.especificacionTecnica,
+      item.uso,
+      item.mantenimiento,
+      item.vidaUtil,
+      item.reposicion,
+      item.disposicionFinal
+    ]), // Datos de la tabla
+    theme: 'grid', // Estilo de la tabla
+    margin: { top: 28.5, left: 5.5, right: 5.5}, // Margen superior para evitar que se solape con el encabezado
+    styles: { fontSize: 8 }, // Estilo de fuente para la tabla
+    headerStyles: {
+      textColor: '#000',
+      fillColor: [200, 200, 200], // Color gris (F4F4F4)
+    },
+    columnWidth: 'auto', // Anchura automática para la primera columna
+    didDrawPage: function () {
+      // Verificar si hay más de una página adicional
+      const totalPages = doc.internal.getNumberOfPages();
+      if (totalPages > firstPagePosition) {
+        // Iterar a través de las páginas adicionales y dibujar el encabezado y los márgenes
+        for (let i = firstPagePosition + 1; i <= totalPages; i++) {
+          doc.setPage(i);
+          drawHeader(doc);
+          drawBorders(doc);
+        }
+      }
+    }
+  });
+
+  doc.save("informacion.pdf");
+}
   }
 }
 </script>
